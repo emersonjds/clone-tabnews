39099a87e761f2a5d90735aff517bd1ee8c9b9a5
Emerson Silva <emerson_jdss@hotmail.com>
2025-08-04 23:52:54 -0300

    remove useEffect

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,6 @@ import React, { useEffect } from "react";
 import { sql } from "bun";
 
 const Home = () => {
-  useEffect(() => {
-    const createTable = async () => {
-      try {
-        await sql`CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)`;
-        console.log("Table created successfully");
-      } catch (error) {
-        console.error("Error creating table:", error);
-      }
-    };
-
-    createTable();
-  }, []);
-
   return <div>Home App</div>;
 };
 
