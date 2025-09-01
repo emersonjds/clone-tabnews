33b33109d9cef862fa04032abaf1b8a8145532e9
Emerson Silva <emerson_jdss@hotmail.com>
2025-08-31 22:55:18 -0300

    change on Home

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
+import React from "react";
+
+function Home() {
+  return (
+    <h1>
+      Data
+    </h1>
+  )
 }
 
-const user = {
-  firstName: 'Harper',
-  lastName: 'Perez'
-};
-
-const element = (
-  <h1>
-    Hello, {formatName(user)}!
-  </h1>
-);
+export default Home;
