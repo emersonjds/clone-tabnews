a09da072b0260e4ba41881d322dc5499659b788b
Emerson Silva <emerson_jdss@hotmail.com>
2025-10-21 22:49:15 -0300

    remove provider

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
 import Counter from "../components/Counter";
-import { Provider } from "jotai";
 
 const Home = () => {
   return (
-    <Provider>
-      <div>
-        <h1>Home Page</h1>
-        <Counter />
-      </div>
-    </Provider>
+    <div>
+      <h1>Home Page</h1>
+      <Counter />
+    </div>
   );
 };
 
