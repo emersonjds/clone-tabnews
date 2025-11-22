5c85f500561f8f40aa4f066d478c0dd33f053ebe
Emerson Silva <emerson_jds@live.com>
2025-11-22 15:36:28 -0300

    remove src

--- src/store/counterStore.js
+++
@@ -1,8 +0,0 @@
-// Store for handling counter state
-
-// Define a simple counter state
-export const counterState = {
-  count: 0,
-  increment: () => counterState.count++,
-  decrement: () => counterState.count--,
-};
