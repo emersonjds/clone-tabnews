f2341e833b1fd89fae4fd49a00c70f50b245e0f0
Emerson Silva <emerson_jdss@hotmail.com>
2025-12-20 19:51:42 -0300

    first integration test

--- pages/api/status.js
+++
@@ -1,5 +0,0 @@
-function status(req, res) {
-  res.status(200).json({ status: 'OK' });
-}
-
-export default status;
--- tests/unit/calc.test.js
+++ tests/unit/calc.test.js
@@ -1,4 +1,4 @@
-const calc = require("../models/calc");
+const calc = require("../../models/calc");
 
 test("sum 2 + 2 should be 4 ", () => {
   const result = calc.sum(2, 2);
