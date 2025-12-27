fc37bbbed74030a87ccb197d7399b64eec9c9b33
Emerson Silva <emerson_jdss@hotmail.com>
2025-12-27 17:45:52 -0300

    database module

---
+++ infra/database.js
@@ -0,0 +1,12 @@
+import { Client } from "pg";
+
+async function query (queryObject) {
+  const client = new Client(); // instance of client for use on connection
+  await client.connect(); // connect on db
+  const result = await client.query(queryObject);
+  await client.end(); // close connection
+}
+
+export default {
+  query: query
+}
