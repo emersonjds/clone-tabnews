7a0245675fbe761c348e93b4843af161837b0099
Emerson Silva <emerson_jdss@hotmail.com>
2025-12-27 19:11:16 -0300

    configurations of db

--- infra/database.js
+++ infra/database.js
@@ -1,7 +1,14 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client(); // instance of client for use on connection
+  // on instance of Client it's pass the configuration of connection
+  const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    database: 'postgres',
+    user: 'postgres',
+    password: '123456',
+  }); // instance of client for use on connection
   await client.connect(); // connect on db
   const result = await client.query(queryObject);
   await client.end(); // close connection
