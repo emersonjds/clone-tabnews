8c653826f380be17524eacf67d73e6e460c52926
Emerson Silva <emerson_jdss@hotmail.com>
2025-12-27 18:52:33 -0300

    adjust jest.cjs

--- infra/database.js
+++ infra/database.js
@@ -1,6 +1,6 @@
 import { Client } from "pg";
 
-async function query (queryObject) {
+async function query(queryObject) {
   const client = new Client(); // instance of client for use on connection
   await client.connect(); // connect on db
   const result = await client.query(queryObject);
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,9 @@
+import database from "../../../../../infra/database";
+
 test("Should return status 200 on GET /api/v1/status", async () => {
+  console.log('Teste 1 ');
+  console.log(database);
+  console.log('Teste');
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
