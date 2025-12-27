fef9a6bcd3d039b34e037a5c5c6d78a947e2c0e3
Emerson Silva <emerson_jdss@hotmail.com>
2025-12-27 19:22:59 -0300

    get result on properties

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -5,7 +5,7 @@ test("Should return status 200 on GET /api/v1/status", async () => {
   const result = await database.query(
     'SELECT 1 + 1 AS result'
   );
-  console.log(result);
+  console.log(result.rows);
 
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
