16fd0293ef90c714212f22e35129ae61e6a63992
Emerson Silva <emerson_jdss@hotmail.com>
2026-01-04 22:40:53 -0300

    remove log from database

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
-import database from "/infra/database";
+import database from "infra/database";
 
 test("Should return status 200 on GET /api/v1/status", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
