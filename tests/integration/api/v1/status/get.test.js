0e3fd2ee986085c38722fb723e7b93eb5aa1467e
Emerson Silva <emerson_jdss@hotmail.com>
2026-01-07 22:34:18 -0300

    adjust unit tests

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,4 +3,7 @@ import database from "infra/database";
 test("Should return status 200 on GET /api/v1/status", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBOdy = await response.json();
+  console.log(responseBOdy);
 });
