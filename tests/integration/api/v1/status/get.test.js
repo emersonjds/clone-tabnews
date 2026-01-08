9bf8202d22aa47bbd200bd75bec57d39852d8812
Emerson Silva <emerson_jdss@hotmail.com>
2026-01-07 23:28:52 -0300

    remove unused variables

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -10,10 +10,6 @@ test("Should return status 200 on GET /api/v1/status", async () => {
   console.log('Data consegue ser parseada', parsedUpdatedAt);
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  // const dbVersion = responseBody.version_pg;
-  // const maxConnections = responseBody.max_connections;
-  // const usedConnections = responseBody.used_connections;
-
   expect(responseBody.version_pg).toBe("16.0");
   expect(responseBody.max_connections).toBeGreaterThan(1);
   expect(responseBody.used_connections).toBeGreaterThanOrEqual(1);
