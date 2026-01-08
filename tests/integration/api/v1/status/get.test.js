be0f52baff00fbb482c0bd721ee9c5beb60d5413
Emerson Silva <emerson_jdss@hotmail.com>
2026-01-07 23:29:36 -0300

    adjusts on code

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,5 @@
 test("Should return status 200 on GET /api/v1/status", async () => {
+
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 
