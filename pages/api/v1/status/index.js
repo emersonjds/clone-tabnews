a01b55784ce99cf6e4be33fcf153c944ed6b045c
Emerson Silva <emerson_jdss@hotmail.com>
2026-01-07 23:10:29 -0300

    data from database

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,39 @@
-function status(request, response) {
-  response.status(200).json({ status: "ok" });
+import { Client } from "pg";
+
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
+  try {
+    await client.connect();
+    const dbVersion = await client.query('SELECT version();');
+    const maxConnectionsResult = await client.query("SHOW max_connections;");
+    const usedConnections = await client.query("SELECT COUNT(*) FROM pg_stat_activity;");
+
+    console.log('Database version:', dbVersion.rows[0].version);
+    console.log('Database max connections', maxConnectionsResult.rows[0].max_connections);
+    console.log('Database usedConnections', usedConnections.rows[0].count)
+
+    response.status(200).json({
+      updated_at: updatedAt,
+      version_pg: '',
+      max_connections: '',
+      used_connections: '',
+    });
+
+  } catch (e) {
+    console.log('Database connection error:', e);
+  } finally {
+    await client.end();
+  }
+
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,6 +4,13 @@ test("Should return status 200 on GET /api/v1/status", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 
-  const responseBOdy = await response.json();
-  console.log(responseBOdy);
+  const responseBody = await response.json();
+  console.log('Response body:', responseBody);
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  console.log('Data consegue ser parseada', parsedUpdatedAt);
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
 });
+
