2409b218b37f866efc51274dcddd5c5fa886f27f
Emerson Silva <emerson_jdss@hotmail.com>
2026-01-07 23:20:39 -0300

    remove unused import

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,3 @@
-import database from "infra/database";
-
 test("Should return status 200 on GET /api/v1/status", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
