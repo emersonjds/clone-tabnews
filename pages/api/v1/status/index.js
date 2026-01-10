387ee27342db0a8a11fa32423f9d4ed2d6511618
Emerson Silva <emerson_jdss@hotmail.com>
2026-01-09 23:20:08 -0300

    remove data of database

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,38 +1,24 @@
 import { Client } from "pg";
 
+import database from "../../../../infra/database.js";
+
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+  const dbVersion = await database.query("SELECT version() AS version_postgres;");
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const usedConnections = await database.query("SELECT COUNT(*) FROM pg_stat_activity;");
+
+  console.log('Database version:', dbVersion.rows[0].version_postgres.split(" ")[1]);
+  console.log('Database max connections', maxConnectionsResult.rows[0].max_connections);
+  console.log('Database usedConnections', usedConnections.rows[0].count)
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    version_pg: dbVersion.rows[0].version_postgres.split(" ")[1],
+    max_connections: Number(maxConnectionsResult.rows[0].max_connections),
+    used_connections: Number(usedConnections.rows[0].count),
   });
-
-  try {
-    await client.connect();
-    const dbVersion = await client.query("SELECT version() AS version_postgres;");
-    const maxConnectionsResult = await client.query("SHOW max_connections;");
-    const usedConnections = await client.query("SELECT COUNT(*) FROM pg_stat_activity;");
-
-    console.log('Database version:', dbVersion.rows[0].version_postgres.split(" ")[1]);
-    console.log('Database max connections', maxConnectionsResult.rows[0].max_connections);
-    console.log('Database usedConnections', usedConnections.rows[0].count)
-
-    response.status(200).json({
-      updated_at: updatedAt,
-      version_pg: dbVersion.rows[0].version_postgres.split(" ")[1],
-      max_connections: Number(maxConnectionsResult.rows[0].max_connections),
-      used_connections: Number(usedConnections.rows[0].count),
-    });
-
-  } catch (e) {
-    console.log('Database connection error:', e);
-  } finally {
-    await client.end();
-  }
 
 }
 
