94dd5cec146746516ed57073e7aed7c58ba71e49
Emerson Silva <emerson_jdss@hotmail.com>
2026-01-09 23:59:16 -0300

    remove unused assertion

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,8 +4,6 @@ test("Should return status 200 on GET /api/v1/status", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  console.log('Response body:', responseBody);
-  expect(responseBody.updated_at).toBeDefined();
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
 
