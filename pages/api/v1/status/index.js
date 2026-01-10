a7bf234f71ce2814cb102b95491c2f5c869a373c
Emerson Silva <emerson_jdss@hotmail.com>
2026-01-09 23:59:06 -0300

    adjust instruction for SHOW

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,7 +6,7 @@ async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
   const dbVersion = await database.query("SELECT version() AS version_postgres;");
-  // its possible to use SHOW too
+  // its possible to use SHOW server_version; as well
   const maxConnectionsResult = await database.query("SHOW max_connections;");
   const usedConnections = await database.query("SELECT COUNT(*) FROM pg_stat_activity;");
 
