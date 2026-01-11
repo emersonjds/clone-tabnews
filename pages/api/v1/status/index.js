b6cd03753a041273c3d6c0e8e1dca68a62682cc0
Emerson Silva <emerson_jdss@hotmail.com>
2026-01-10 22:11:36 -0300

    log data from postgres

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -10,9 +10,12 @@ async function status(request, response) {
   const maxConnectionsResult = await database.query("SHOW max_connections;");
   const usedConnections = await database.query("SELECT COUNT(*) FROM pg_stat_activity;");
 
-  console.log('Database version:', dbVersion.rows[0].version_postgres.split(" ")[1]);
-  console.log('Database max connections', maxConnectionsResult.rows[0].max_connections);
-  console.log('Database usedConnections', usedConnections.rows[0].count)
+  const getConnections = await database.query("SELECT * FROM pg_stat_activity WHERE datname='postgres';");
+  console.log('Current connections to local_db:', getConnections.rows);
+
+  // console.log('Database version:', dbVersion.rows[0].version_postgres.split(" ")[1]);
+  // console.log('Database max connections', maxConnectionsResult.rows[0].max_connections);
+  // console.log('Database usedConnections', usedConnections.rows[0].count)
 
   response.status(200).json({
     updated_at: updatedAt,
