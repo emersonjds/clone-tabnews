abdb181fee48337cd656bbbcd551e03173294f9e
Emerson Silva <emerson_jdss@hotmail.com>
2026-01-10 22:21:02 -0300

    type casting in db

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -8,7 +8,7 @@ async function status(request, response) {
   const dbVersion = await database.query("SELECT version() AS version_postgres;");
   // its possible to use SHOW server_version; as well
   const maxConnectionsResult = await database.query("SHOW max_connections;");
-  const usedConnections = await database.query("SELECT COUNT(*) FROM pg_stat_activity;");
+  const usedConnections = await database.query("SELECT COUNT(*)::int FROM pg_stat_activity;");
 
   const getConnections = await database.query("SELECT * FROM pg_stat_activity WHERE datname='postgres';");
   console.log('Current connections to local_db:', getConnections.rows);
@@ -21,7 +21,7 @@ async function status(request, response) {
     updated_at: updatedAt,
     version_pg: dbVersion.rows[0].version_postgres.split(" ")[1],
     max_connections: Number(maxConnectionsResult.rows[0].max_connections),
-    used_connections: Number(usedConnections.rows[0].count),
+    used_connections: usedConnections.rows[0].count,
   });
 
 }
