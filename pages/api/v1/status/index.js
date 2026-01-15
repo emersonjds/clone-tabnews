7eb2c4401f0494be0e8ff9663c27103bdbcea657
Emerson Silva <emerson_jdss@hotmail.com>
2026-01-14 22:44:15 -0300

    remove console from db

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -11,7 +11,7 @@ async function status(request, response) {
   const dbName = 'local_db';
   //Access data on query params
   const dbNameByQuery = request.query.dbName;
-  console.log(`Database name ${dbNameByQuery}`);
+  // console.log(`Database name ${dbNameByQuery}`);
   // const getConnections = await database.query(`SELECT * FROM pg_stat_activity WHERE datname='${dbName}';`);
   const getConnections = await database.query("SELECT * FROM" + " pg_stat_activity WHERE datname='postgres';");
   // console.log('Current connections to local_db:', getConnections.rows);
@@ -19,7 +19,7 @@ async function status(request, response) {
   // console.log('Database version:', dbVersion.rows[0].version_postgres.split(" ")[1]);
   // console.log('Database max connections', maxConnectionsResult.rows[0].max_connections);
   // console.log('Database usedConnections', usedConnections.rows[0].count)
-  
+
   response.status(200).json({
     updated_at: updatedAt,
     version_pg: dbVersion.rows[0].version_postgres.split(" ")[1],
