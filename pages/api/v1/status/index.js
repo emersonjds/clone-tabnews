0ccae054a1cee34742fd95789dacf409a1e13a58
Emerson Silva <emerson_jdss@hotmail.com>
2026-01-15 23:59:46 -0300

    adjust comma

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -18,7 +18,7 @@ async function status(request, response) {
 
   response.status(200).json({
     updated_at: updatedAt,
-    version_pg: dbVersion.rows[0].version_postgres.split(" ")[1]
+    version_pg: dbVersion.rows[0].version_postgres.split(" ")[1],
     max_connections: Number(maxConnectionsResult.rows[0].max_connections),
     used_connections: usedConnections.rows[0].count,
   });
