b261cf31c58f4d0f9d5df74af9d0eae7a1b82ce4
Emerson Silva <emerson_jdss@hotmail.com>
2026-01-20 00:30:02 -0300

    adjust configurations to get db data

--- infra/database.js
+++ infra/database.js
@@ -1,31 +1,20 @@
 import {Client} from "pg";
 
 async function query(queryObject) {
-  // on instance of Client it's pass the configuration of connection
   const client = new Client({
     host: process.env.PRD_HOST,
-    port: process.env.PRD_PORT,
+    port: parseInt(process.env.PRD_PORT, 10),
     user: process.env.PRD_USER,
     database: process.env.PRD_DB,
     password: process.env.PRD_PASSWORD,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  }); // instance of client for use on connection
-
-  // console.log('Return of db...',{
-  //   host: process.env.POSTGRES_HOST,
-  //   port: process.env.POSTGRES_PORT,
-  //   user: process.env.POSTGRES_USER,
-  //   database: process.env.POSTGRES_DB,
-  //   password: process.env.POSTGRES_PASSWORD,
-  // });
+    ssl: true
+  });
 
   try {
-    await client.connect(); // connect on db
+    await client.connect();
     return await client.query(queryObject);
   } catch (error) {
-    console.log('Database query error:', error);
+    console.error('Database query error:', error.message);
     throw error;
   } finally {
     await client.end();
