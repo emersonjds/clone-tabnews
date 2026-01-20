6d561e61d978aa7ecf6ccd459dfe41485815ed96
Emerson Silva <emerson_jdss@hotmail.com>
2026-01-19 23:55:12 -0300

    adjust .env production

--- infra/database.js
+++ infra/database.js
@@ -1,22 +1,22 @@
-import {Client} from "pg";
+import { Client } from "pg";
 
 async function query(queryObject) {
   // on instance of Client it's pass the configuration of connection
   const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.PRD_HOST,
+    port: process.env.PRD_PORT,
+    user: process.env.PRD_USER,
+    database: process.env.PRD_DB,
+    password: process.env.PRD_PASSWORD,
   }); // instance of client for use on connection
 
-  console.log('Return of db...',{
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
+  // console.log('Return of db...',{
+  //   host: process.env.POSTGRES_HOST,
+  //   port: process.env.POSTGRES_PORT,
+  //   user: process.env.POSTGRES_USER,
+  //   database: process.env.POSTGRES_DB,
+  //   password: process.env.POSTGRES_PASSWORD,
+  // });
 
   try {
     await client.connect(); // connect on db
