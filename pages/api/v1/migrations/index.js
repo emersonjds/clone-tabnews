9869fadd0543d55f2dba617adf8abefb5c34bdb3
Emerson Silva <emerson_jdss@hotmail.com>
2026-02-01 00:15:09 -0300

    prepare return with validate request method

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,18 +3,32 @@ import { join } from 'node:path';
 
 export default async function migrations(request, response) {  
 
-  const migrations = await migrationRunner({
-    databaseUrl: process.env.DATABASE_URL,
-    direction: "up",
-    dir: join("infra", "migrations"),
-    dryRun: true,
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  });
+  if (request.method === "POST") {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      direction: "up",
+      dir: join("infra", "migrations"),
+      dryRun: false,
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+    return response.status(200).json(migrations);
+  } 
+  if (request.method === "GET")  {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      direction: "up",
+      dir: join("infra", "migrations"),
+      dryRun: true,
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+    return response.status(200).json(migrations);
+  }
 
   console.log(migrations);
-
-  return response.status(200).json(migrations);
+  return response.status(405).end();
+  // return response.status(200).json(migrations);
 }
 
 
