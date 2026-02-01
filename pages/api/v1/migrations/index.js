d1d63a0fd58ebc17918aef7ab0cd6691757d0817
Emerson Silva <emerson_jdss@hotmail.com>
2026-01-31 22:55:39 -0300

    initial tests with form of response

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,5 +1,5 @@
 export default async function migrations(request, response) {  
-  return response.status(200).json({});
+  return response.status(200).json([]);
 }
 
 
--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -3,7 +3,9 @@ test("Should return status 200 on GET /api/v1/migrations", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
-  // const responseBody = await response.json();
+  const responseBody = await response.json();
+
+  expect(Array.isArray(responseBody)).toBe(true);
 });
 
 
