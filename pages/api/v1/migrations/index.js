239923969f2ea79ecdf8874b03c0900838f35fb2
Emerson Silva <emerson_jdss@hotmail.com>
2026-01-31 22:49:33 -0300

    adjust error on default export

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,6 +1,6 @@
-export async function migrations(request, response) {
-  
-  response.status(200).json({});
+export default async function migrations(request, response) {  
+  return response.status(200).json({});
 }
+
 
 
