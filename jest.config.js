b2a041f44bb4ba846007c37ca717669c86442419
Emerson Silva <emerson_jdss@hotmail.com>
2026-02-03 00:55:37 -0300

    jest config esm and cjs

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
-const nextJest = require('next.jest');
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest();
-const jestConfig = createJestConfig();
+const jestConfig = createJestConfig({
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+});
 
-module.exports =  jestConfig;
+module.exports = jestConfig;
