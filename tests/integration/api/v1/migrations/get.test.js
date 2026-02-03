5a34c128d3f8cef8430a22b5c8fd8bea073957c3
Emerson Silva <emerson_jdss@hotmail.com>
2026-02-03 00:55:45 -0300

    challenge

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,3 +1,5 @@
+import database from 'infra/database.js';
+
 test("Should return status 200 on GET /api/v1/migrations", async () => {
 
   const response = await fetch("http://localhost:3000/api/v1/migrations");
@@ -5,6 +7,12 @@ test("Should return status 200 on GET /api/v1/migrations", async () => {
 
   const responseBody = await response.json();
   console.log(responseBody);
+
+  console.log('environment return', process.env.NODE_ENV);
+  console.log('database url:', process.env.DATABASE_URL);
+
+  expect(process.env.NODE_ENV).toBe('test');
+  expect(process.env.DATABASE_URL).toBeUndefined();
 
   expect(Array.isArray(responseBody)).toBe(true);
   expect(responseBody.length).toBeGreaterThan(0);
