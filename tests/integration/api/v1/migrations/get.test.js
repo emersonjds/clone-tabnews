2d6975ad95036d777916143b1f9235f6493d93f2
Emerson Silva <emerson_jdss@hotmail.com>
2026-02-04 20:46:44 -0300

    adjust tests on post

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,20 +1,16 @@
 import database from 'infra/database.js';
 
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+beforeAll(cleanDatabase);
+
 test("Should return status 200 on GET /api/v1/migrations", async () => {
-
-  await database.query({ text: 'SELECT 1 + 1' });
-
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  console.log(responseBody);
-
-  console.log('environment return', process.env.NODE_ENV);
-  console.log('database url:', process.env.DATABASE_URL);
-
-  expect(process.env.NODE_ENV).toBe('test');
-  expect(process.env.DATABASE_URL).toBeDefined();
 
   expect(Array.isArray(responseBody)).toBe(true);
   expect(responseBody.length).toBeGreaterThan(0);
