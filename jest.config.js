07bfd992a3c567ef116b84024b4f845b2127f887
Emerson Silva <emerson_jdss@hotmail.com>
2026-02-04 20:20:02 -0300

    adjust dir on nextJest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 const nextJest = require('next/jest');
 
-const createJestConfig = nextJest();
+const createJestConfig = nextJest({
+  dir: '.',
+});
 const jestConfig = createJestConfig({
   moduleDirectories: ['node_modules', '<rootDir>/'],
 });
