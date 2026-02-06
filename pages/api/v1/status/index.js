eaf5691e95d7edd0dec0448d54382b06c89a2edc
Emerson Silva <emerson_jdss@hotmail.com>
2026-02-05 23:19:24 -0300

    changes on database.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,14 +3,18 @@ import database from "../../../../infra/database.js";
 export default async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const dbVersion = await database.query("SELECT version() AS version_postgres;");
+  const dbVersion = await database.query(
+    "SELECT version() AS version_postgres;",
+  );
   const maxConnectionsResult = await database.query("SHOW max_connections;");
-  const usedConnections = await database.query("SELECT COUNT(*)::int FROM pg_stat_activity;");
+  const usedConnections = await database.query(
+    "SELECT COUNT(*)::int FROM pg_stat_activity;",
+  );
 
   const dbName = process.env.DB_NAME;
   const getConnections = await database.query({
     text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1",
-    values: [dbName]
+    values: [dbName],
   });
 
   response.status(200).json({
@@ -19,7 +23,4 @@ export default async function status(request, response) {
     max_connections: Number(maxConnectionsResult.rows[0].max_connections),
     used_connections: usedConnections.rows[0].count,
   });
-
 }
-
-
