0f365d5135e5c74595d528b27c9a68dd5a04ee60
Emerson Silva <emerson_jdss@hotmail.com>
2026-02-07 23:59:23 -0300

    changes on return

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -28,5 +28,5 @@ export default async function migrations(request, response) {
 
   console.log(migrations);
   return response.status(405).end();
-  // return response.status(200).json(migrations);
+  return response.status(200).json(migrations);
 }
