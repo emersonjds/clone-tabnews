ff10030cda8f489cb3a3adf4cb159722fbd520f3
Emerson Silva <emerson_jdss@hotmail.com>
2026-02-24 00:38:49 -0300

    remove log from date

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -7,10 +7,7 @@ test("Should return status 200 on GET /api/v1/status", async () => {
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
 
-  console.log('Data consegue ser parseada', parsedUpdatedAt);
-
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-
   expect(responseBody.version_pg).toBe("16.0");
   expect(responseBody.max_connections).toBeGreaterThan(1);
   expect(responseBody.used_connections).toBeGreaterThanOrEqual(1);
