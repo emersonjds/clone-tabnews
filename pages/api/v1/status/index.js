ed711cec92dbfb86f83d31a6727cffb9fc9aa46e
Emerson Silva <emerson_jdss@hotmail.com>
2026-02-24 01:12:05 -0300

    refactor on status controller

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,19 +4,15 @@ export default async function status(request, response) {
 
   const updatedAt = new Date().toISOString();
 
-  const dbVersion = await database.query(
-    "SELECT version() AS version_postgres;",
-  );
-  const maxConnectionsResult = await database.query("SHOW max_connections;");
-  const usedConnections = await database.query(
-    "SELECT COUNT(*)::int FROM pg_stat_activity;",
-  );
-
-  const dbName = process.env.DB_NAME;
-  const getConnections = await database.query({
-    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1",
-    values: [dbName],
-  });
+  const [dbVersion, maxConnectionsResult, usedConnections, getConnections] = await Promise.all([
+    database.query("SELECT version() AS version_postgres;"),
+    database.query("SHOW max_connections;"),
+    database.query("SELECT COUNT(*)::int FROM pg_stat_activity;"),
+    database.query({
+      text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1",
+      values: [process.env.POSTGRES_DB],
+    }),
+  ]);
 
   response.status(200).json({
     updated_at: updatedAt,
