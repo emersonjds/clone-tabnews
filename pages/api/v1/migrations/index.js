831c043b8d64a94b92fa69b420be9a0968b8dbb8
Emerson Silva <emerson_jdss@hotmail.com>
2026-02-24 00:54:54 -0300

    refactor controller

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -32,11 +32,12 @@ export default async function migrations(request, response) {
       });
       return response.status(200).json(pendingMigrations);
     }
+
     if (request.method === "POST") {
       const migratedMigrations = await migrationRunner(defaultMigration);
       return response.status(201).json(migratedMigrations);
     }
-    return response.status(405).end();
+
   } catch (error) {
     console.error("Error running migrations:", error);
     throw error;
