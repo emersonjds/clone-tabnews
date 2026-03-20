a115ceaf2034d0ee8321f0932d34adb3163c4b4b
Emerson Silva <emerson_jdss@hotmail.com>
2026-03-19 22:38:26 -0300

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1>Continue.. vai valer a pena. Há pessoas que se inspiram em voce</h1>
+      <h1>Continue... vai valer a pena. Há pessoas que se inspiram em voce</h1>
     </>
   );
 }
