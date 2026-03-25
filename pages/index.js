935e15ab6f9635c3b2c433893824aa770e41914a
Emerson Silva <emerson_jdss@hotmail.com>
2026-03-24 23:33:39 -0300

    add milestone

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <>
       <h1>Continue... Vai valer a pena. Há pessoas que se inspiram em voce</h1>
+        <h1>Create milestone</h1>
     </>
   );
 }
